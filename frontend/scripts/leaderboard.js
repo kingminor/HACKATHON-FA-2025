9b725cdd8e78efa7c308a26d0364bdeace6a2741
Willkjj <willkjj@gmail.com>
2025-10-25 01:52:54 -0600

    leaderboard finish?

---
+++ frontend/scripts/leaderboard.js
@@ -0,0 +1,31 @@
+let selectedLeaderboard = "group";
+
+document.querySelectorAll(".leaderboard").forEach((el) => {
+    el.addEventListener("click", (e) => {
+        selectedLeaderboard = switchLeaderboard(e);
+    })
+})
+
+
+function switchLeaderboard(e) {
+    const selectedElement = e.target.closest(".leaderboard");
+    const leaderboardId = selectedElement.classList[1];
+
+    if(leaderboardId === "group") {
+        document.querySelector(".groupGlobal").id = "";
+        document.querySelector(".playerGlobal").id = "";
+        document.querySelector(".group").id = "selected";
+    } else if (leaderboardId === "groupGlobal") {
+        document.querySelector(".group").id = "";
+        document.querySelector(".playerGlobal").id = "";
+        document.querySelector(".groupGlobal").id = "selected";
+    } else {
+        document.querySelector(".groupGlobal").id = "";
+        document.querySelector(".group").id = "";
+        document.querySelector(".playerGlobal").id = "selected";
+    }
+
+    return leaderboardId;
+
+    console.log(leaderboardId);
+}
